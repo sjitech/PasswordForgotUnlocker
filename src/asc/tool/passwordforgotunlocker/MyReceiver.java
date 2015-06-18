c04e4caf8df517269d776a10853e7a65cf875b74
Hirokazu Mori <mori-hirokazu@sji-inc.jp>
2015-06-18 12:25:21 +0900

    spell miss

--- src/asc/tool/passwordforgotunlocker/MyReceiver.java
+++ src/asc/tool/passwordforgotunlocker/MyReceiver.java
@@ -3,13 +3,20 @@ package asc.tool.passwordforgotunlocker;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.util.Log;
 
 public class MyReceiver extends BroadcastReceiver {
 
 	@Override
-	public void onReceive(Context context, Intent intent) {
+	public void onReceive(final Context context, Intent intent) {
 		Log.d("ASC", "got event");
-		context.startService(new Intent(context, PasswordForgotUnlockerService.class));
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				context.startService(new Intent(context, PasswordForgotUnlockerService.class));
+			}
+		}, 10 * 1000);
 	}
 }
